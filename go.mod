eaeef080c0abf8525d86e53c97c3ee9f733efb2e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-01-07 16:49:51 +0000

    Add a basic broker implementation
    This is the minimal config and code required to implement the service
    broker interface and compile a broker server. No operations are
    currently supported.
    
    We choose to import the broker as a library. This is because a lot of
    the implementation is the same across brokers, and therefore can be
    shared.
    
    (Thanks @henrytk :smile:)

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/alphagov/paas-drone-agent-broker
+
+require (
+	code.cloudfoundry.org/lager v2.0.0+incompatible
+	github.com/alphagov/paas-go v0.0.0-20181008075827-0065fc706364
+	github.com/drewolson/testflight v1.0.0 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/onsi/ginkgo v1.7.0 // indirect
+	github.com/onsi/gomega v1.4.3 // indirect
+	github.com/pborman/uuid v1.2.0 // indirect
+	github.com/pivotal-cf/brokerapi v3.0.2+incompatible
+	github.com/pkg/errors v0.8.1 // indirect
+)
---
+++ provider/provider.go
@@ -0,0 +1,44 @@
+package provider
+
+import (
+	"github.com/pivotal-cf/brokerapi"
+	provideriface "github.com/alphagov/paas-go/provider"
+	"context"
+	"errors"
+)
+
+type DroneAgentProvider struct {}
+
+func NewDroneAgentProvider(config []byte) (provideriface.ServiceProvider, error) {
+	return &DroneAgentProvider{}, nil
+}
+
+func (s *DroneAgentProvider) Provision(ctx context.Context, provisionData provideriface.ProvisionData) (
+	dashboardURL, operationData string, isAsync bool, err error) {
+	return "", "", false, errors.New("not implemented")
+}
+
+func (s *DroneAgentProvider) Deprovision(ctx context.Context, deprovisionData provideriface.DeprovisionData) (
+	operationData string, isAsync bool, err error) {
+	return "", false, errors.New("not implemented")
+}
+
+func (s *DroneAgentProvider) Bind(ctx context.Context, bindData provideriface.BindData) (
+	binding brokerapi.Binding, err error) {
+		return brokerapi.Binding{}, errors.New("not implemented")
+}
+
+func (s *DroneAgentProvider) Unbind(ctx context.Context, unbindData provideriface.UnbindData) (
+	spec brokerapi.UnbindSpec, err error) {
+		return brokerapi.UnbindSpec{}, errors.New("not implemented")
+}
+
+func (s *DroneAgentProvider) Update(ctx context.Context, updateData provideriface.UpdateData) (
+	operationData string, isAsync bool, err error) {
+		return "", false, errors.New("not implemented")
+}
+
+func (s *DroneAgentProvider) LastOperation(ctx context.Context, lastOperationData provideriface.LastOperationData) (
+	state brokerapi.LastOperationState, description string, err error) {
+		return "", "", errors.New("not implemented")
+}
