d0afa05489e5f2baebb78290ad932d2892cf1bb4
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2019-01-08 11:11:07 +0000

    WIP: flesh out EC2 client

--- ec2/client.go
+++ ec2/client.go
@@ -11,27 +11,27 @@ import (
 
 //go:generate counterfeiter -o fakes/fake_ec2_client.go . Client
 type Client interface {
-	//CreateBucket(name string) error
+	RunEC2(input ec2.RunInstancesInput) error
 	//DeleteBucket(name string) error
 	//AddUserToBucket(username, bucketName string) (BucketCredentials, error)
 	//RemoveUserFromBucket(username, bucketName string) error
 }
 
-type BucketCredentials struct {
-	AWSAccessKeyID     string `json:"aws_access_key_id"`
-	AWSSecretAccessKey string `json:"aws_secret_access_key"`
-}
-
 type EC2Client struct {
-	Timeout      time.Duration
-	EC2          ec2iface.EC2API
+	Timeout time.Duration
+	EC2     ec2iface.EC2API
 }
 
 func NewEC2Client(bucketPrefix, region string) *EC2Client {
 	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String(region)}))
 	ec2Client := ec2.New(sess)
 	return &EC2Client{
-		Timeout:      30 * time.Second,
-		EC2:          ec2Client,
+		Timeout: 30 * time.Second,
+		EC2:     ec2Client,
 	}
+}
+
+func (s *EC2Client) RunEC2(input ec2.RunInstancesInput) error {
+	_, err := s.EC2.RunInstances(&input)
+	return err
 }
